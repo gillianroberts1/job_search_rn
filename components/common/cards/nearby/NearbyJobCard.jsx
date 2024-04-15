3fcaaaa908718097b18eaeca29fef76664c0e358
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-04-15 17:01:51 +0100

    job details added

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -13,7 +13,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
               ? job.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
-          resizeMode='contain'
+          resizeMode="contain"
           style={styles.logImage}
         />
       </TouchableOpacity>
@@ -23,7 +23,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
           {job?.job_title}
         </Text>
 
-        <Text style={styles.jobType}>{job?.job_employment_type}</Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
       </View>
     </TouchableOpacity>
   );
